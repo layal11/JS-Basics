e9548961083106afa0b03a16329443342f13165b
Layal Haidar <layal.haidar1@hotmail.com>
2021-05-29 20:45:09 +0300

    step_6

--- javascript-basics-01/Step_6.js
+++ javascript-basics-01/Step_6.js
@@ -2,6 +2,14 @@ var num1 = document.getElementById("first_number");
 var num2 = document.getElementById("second_number");
 
 function remainder(){
-    if(num1.value != "" && )
-
+    if(!isNaN(num1.value) && !isNaN(num2.value) && num1.value != "" && num2.value != "" ){
+        if(num2.value != "0"){
+        var division = num1.value / num2.value;
+        alert(num1.value % num2.value);
+        } else {
+            alert("denominator can't be 0");
+        }
+    }else{
+        alert("Both must be numbers");
+    }
 }
