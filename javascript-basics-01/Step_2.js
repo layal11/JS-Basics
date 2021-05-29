34282960d45a6dd48f114580a8757fe73896d4ce
Layal Haidar <layal.haidar1@hotmail.com>
2021-05-29 11:57:52 +0300

    step_2

---
+++ javascript-basics-01/Step_2.js
@@ -0,0 +1,13 @@
+ 
+
+//  MESSAGE BOX
+function displayname(){
+ var name = "Layal";
+ var surname = "Haidar";
+ var city = "Beirut"; 
+
+ alert("Name: " + name + "\n surname: " + surname + "\n City: " + city);
+ 
+}
+
+displayname();
