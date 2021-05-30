2fc95f9b6a19b18d48aec3730bf5300e48d7c1b2
Layal Haidar <layal.haidar1@hotmail.com>
2021-05-30 20:53:52 +0300

    step_2

---
+++ javascript-basics-03/step 2/Step_2.js
@@ -0,0 +1,8 @@
+function showhide(flag){
+    if(flag == "1"){
+        document.querySelector("#texte").style.display = "block";
+    }else {
+        document.querySelector("#texte").style.display = "none";
+
+    }
+}
