a21cd4d036e90ae763dba476eb59520960c50c85
Layal Haidar <layal.haidar1@hotmail.com>
2021-05-30 11:18:05 +0300

    step_2

---
+++ javascript-basics-02/step 2/Step_2.js
@@ -0,0 +1,9 @@
+
+// var qselector = document.querySelector("#name").addEventListener('focusout' ,function(){
+//     alert("Thank you for participating!");
+// } );
+
+
+function blurfunction(){
+    alert("Thank you for participating!") ;
+}
