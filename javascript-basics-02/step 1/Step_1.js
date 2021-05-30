881d4f84110bb1cd3f9f8a77621b833fe5a302d9
Layal Haidar <layal.haidar1@hotmail.com>
2021-05-30 10:39:30 +0300

    step_1

---
+++ javascript-basics-02/step 1/Step_1.js
@@ -0,0 +1,18 @@
+
+// var imgselector = document.querySelector("#image1").addEventListener('mouseover' , function(){
+//     this.src = "images/image1_2.jpg";
+// });
+
+// var anotherimgselector = document.querySelector("#image1").addEventListener('mouseout' , function(){
+//     this.src = "images/image1.jpg"
+// });
+
+
+
+function setnewimage(){
+    document.getElementById("image1").src = "images/image1_2.jpg";
+}
+
+function setoldimage(){
+    document.getElementById("image1").src = "images/image1.jpg";
+}
