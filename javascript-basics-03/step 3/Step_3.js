74f4c203c3b73809d3e4eb19fa719ee9d54c29e7
Layal Haidar <layal.haidar1@hotmail.com>
2021-05-30 21:04:41 +0300

    step_3

---
+++ javascript-basics-03/step 3/Step_3.js
@@ -0,0 +1,18 @@
+
+
+function colors(flag){
+var text = document.querySelector("#text");
+
+    switch(flag){
+    case "green":
+        text.style.color = "green";
+        break;
+        case "red":
+        text.style.color = "red";
+        break;
+        case "blue":
+        text.style.color = "blue";
+        break;
+}
+
+}
